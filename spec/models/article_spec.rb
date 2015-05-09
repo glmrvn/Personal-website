d967d8ac23b181e8167c998272e3f75b36dc9fcd
Alex from Ubuntu laptop <faqmybrain@gmail.com>
2015-05-09 22:18:22 +0300

    Fix capybara routing matches

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 describe Article do
 
-  article = Article.create(title: "Title here",
-                           text: "Text here",
-                           image_file_name: "file_name.png")
+  before do
+    @article = Article .create(title: "Title here",
+                              text: "Text here",
+                              image_file_name: "file_name.png")
+  end
 
   it "should be valid Article model" do
-    expect(article).to be_valid
+    expect(@article).to be_valid
   end
 
-  it "should be have h1 eq title in article page" do
-    visit '/articles/1' 
-    expect(page).to have_content(article.title)
+  it "should be have title in article page" do
+    visit articles_path
+    expect(page).to have_content(@article.title)
   end
-
-
+  
 end
