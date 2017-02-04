55baf71df8246bb3e170909f0eb6a997461d83a9
Alex Arnis <glmrvn@gmail.com>
2017-02-04 21:00:30 +0300

    Rails 5.0.1 & Ruby 2.3.3

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
-ruby '2.3.1'
+ruby '2.3.3'
 
-gem 'rails', '5.0.0'
+gem 'rails', '5.0.1'
 gem 'pg'
 
 gem 'puma'
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -37,8 +36,7 @@ ActiveRecord::Schema.define(version: 20160606190547) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "remember_token"
+    t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
-
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
